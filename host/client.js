1fd914c11224bd20f69d4b4072c2d44821e1dae3
Damien Rajon <145502+pyrho@users.noreply.github.com>
2020-03-25 14:04:21 +0100

    feat: activate ff

--- host/client.js
+++ host/client.js
@@ -1,78 +1,93 @@
- const ipc = require('node-ipc');
- const config = require('./config');
- const alfy = require('alfy');
+const ipc = require('node-ipc');
+const config = require('./config');
+const alfy = require('alfy');
+const runApplescript = require('run-applescript');
 
- function sendMessageToHostApp(message) {
-     return new Promise((resolve, reject) => {
+function sendMessageToHostApp(message) {
+    return new Promise((resolve, reject) => {
+        ipc.config.id = 'client';
+        ipc.config.retry = 1500;
+        ipc.config.silent = true;
 
-         ipc.config.id   = 'client';
-         ipc.config.retry= 1500;
-         ipc.config.silent = true;
+        ipc.serveNet(config.ipc.clientPort, 'udp4', function() {
+            const timer = setTimeout(() => {
+                console.error('Request timeout');
+                ipc.server.stop();
+                return reject(new Error('Request timeout'));
+            }, 1000);
 
-         ipc.serveNet(config.ipc.clientPort, 'udp4', function() {
+            ipc.server.on('message', function(data) {
+                clearTimeout(timer);
+                const t = data.message;
+                ipc.server.stop();
+                return resolve(t);
+            });
 
-             const timer = setTimeout(() => {
-                 console.error('Request timeout');
-                 ipc.server.stop();
-                 return reject(new Error('Request timeout'));
-             }, 1000);
+            ipc.server.emit(
+                {
+                    address: '127.0.0.1', //any hostname will work
+                    port: config.ipc.serverPort,
+                },
+                'message',
+                {
+                    from: ipc.config.id,
+                    message,
+                },
+            );
+        });
 
-             ipc.server.on('message', function (data) {
-                 clearTimeout(timer);
-                 const t = data.message;
-                 ipc.server.stop();
-                 return resolve(t);
-             });
+        ipc.server.start();
+    });
+}
 
-             ipc.server.emit({
-                 address : '127.0.0.1', //any hostname will work
-                 port    : config.ipc.serverPort,
-             },
-             'message',
-             {
-                 from    : ipc.config.id,
-                 message,
-             });
-         });
+async function main() {
+    const cmd = process.argv[2];
+    const arg = process.argv[3];
+    switch (cmd) {
+        case 'get':
+            const d = await sendMessageToHostApp('getTabList');
 
-         ipc.server.start();
-     });
+            const formattedElements = d.map(element => ({
+                title: element.title,
+                subtitle: element.url,
+                arg: `${Number(element.id)}:${element.windowId}`,
+            }));
 
- }
+            const filters = process.argv.slice(3).map(a => a.toLowerCase());
+            const titleMatchesAllFilters = e =>
+                filters.every(f => e.title.toLowerCase().indexOf(f) >= 0);
+            const urlMatchesAllFilters = e =>
+                filters.every(f => e.subtitle.toLowerCase().indexOf(f) >= 0);
 
- async function main() {
-     const cmd = process.argv[2];
-     const arg = process.argv[3];
-     switch (cmd) {
-         case 'get':
-             const d = await sendMessageToHostApp('getTabList');
+            const filterItemsIfNeeded = () =>
+                filters
+                    ? formattedElements.filter(
+                          e => titleMatchesAllFilters(e) || urlMatchesAllFilters(e),
+                      )
+                    : formattedElements;
 
-             const formattedElements = d.map(element => ({
-                 title: element.title,
-                 subtitle: element.url,
-                 arg: `${Number(element.id)}:${element.windowId}`,
-             }));
+            alfy.output(filterItemsIfNeeded(formattedElements));
 
-             const filters = process.argv.slice(3).map(a => a.toLowerCase());
-             const titleMatchesAllFilters = e => filters.every(f => e.title.toLowerCase().indexOf(f) >= 0);
-             const urlMatchesAllFilters = e => filters.every(f => e.subtitle.toLowerCase().indexOf(f) >= 0);
+            break;
 
-             const filterItemsIfNeeded = () => filters ?
-                 formattedElements.filter(e => titleMatchesAllFilters(e) || urlMatchesAllFilters(e)) :
-                 formattedElements;
+        case 'switch':
+            sendMessageToHostApp(`switchTo:${arg}`); //.then(d => console.log(d));
+            await runApplescript(`set devEditionExists to false
+try
+	do shell script "osascript -e 'exists application \"Firefox Developer Edition\"'"
+	set devEditionExists to true
+end try
 
-             alfy.output(filterItemsIfNeeded(formattedElements));
+if devEditionExists then
+	tell application "Firefox Developer Edition" to activate
+else
+	tell application "Firefox" to activate
+end if`);
+            break;
 
-             break;
+        default:
+            throw new Error(`Unknown command ${cmd}`);
+    }
+}
 
-   case 'switch':
-       sendMessageToHostApp(`switchTo:${arg}`); //.then(d => console.log(d));
-       break;
-
-   default:
-       throw new Error(`Unknown command ${cmd}`);
-     }
-
- }
-
- main();
+main();
